d2e7e672b075792b88a903bad521b58bd97bf06c
nitram509 <maki@bitkings.de>
2013-11-25 23:21:02 +0100

    extracted sourceReadin method added code for writing .java source files

--- smali2java/src/com/litecoding/smali2java/Ecosystem.java
+++ smali2java/src/com/litecoding/smali2java/Ecosystem.java
@@ -1,57 +1,81 @@
 package com.litecoding.smali2java;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.litecoding.smali2java.entity.smali.SmaliClass;
 import com.litecoding.smali2java.parser.Parser;
 import com.litecoding.smali2java.parser.Rule;
 import com.litecoding.smali2java.renderer.ClassRenderer;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ecosystem
 {
 	private static Ecosystem instance = null;
-	
+
 	private Map<String, SmaliClass> classes = new HashMap<String, SmaliClass>();
-	
+
 	protected Ecosystem()
 	{
-		
+
 	}
-	
+
 	public static Ecosystem getInstance()
 	{
-		if(instance == null)
+		if (instance == null)
 			instance = new Ecosystem();
 		return instance;
 	}
-	
+
 	public Map<String, SmaliClass> getClasses()
 	{
 		return classes;
 	}
-	
+
 	public void processFile(String src, String dst) throws Exception
 	{
 		File srcFile = new File(src);
-		
-		BufferedReader in = new BufferedReader(new FileReader(srcFile));
-	    int ch = 0;
-	    StringBuffer out = new StringBuffer();
-	    while ((ch = in.read()) != -1)
-	      out.append((char)ch);
 
-	    out.append("\n"); //fix for the bug than .end method ends by EOF but not CRLF
-	    in.close();
-		
-		Rule classrule = Parser.parse("smali", out.toString());
-		SmaliClass smaliClass = (SmaliClass)classrule.accept(new SmaliClassBuilder());
+		String smaliSource = readSourceFile(srcFile);
+
+		Rule classrule = Parser.parse("smali", smaliSource);
+		SmaliClass smaliClass = (SmaliClass) classrule.accept(new SmaliClassBuilder());
 		classes.put(smaliClass.getClassName(), smaliClass);
-		
-		System.out.println(ClassRenderer.renderObject(smaliClass));
+
+		String javaSource = ClassRenderer.renderObject(smaliClass);
+
+		printSource(javaSource);
+		writeJavaSourceToFile(dst, javaSource);
 	}
-	
+
+	private String readSourceFile(File srcFile) throws IOException
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(srcFile));
+		char[] buffer = new char[4096];
+		int read = 0;
+		StringBuilder sb = new StringBuilder();
+		while ((read = reader.read(buffer)) > 0)
+		{
+			sb.append(buffer, 0, read);
+		}
+		sb.append("\n"); //fix for the bug than .end method ends by EOF but not CRLF
+		reader.close();
+		return sb.toString();
+	}
+
+	private void printSource(String text)
+	{
+		System.out.println(text);
+	}
+
+	private void writeJavaSourceToFile(String dst, String javaSource) throws IOException
+	{
+		if (dst != null)
+		{
+			FileWriter writer = new FileWriter(dst);
+			writer.write(javaSource);
+			writer.close();
+		}
+	}
+
 }
