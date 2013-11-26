197f5a611718427c9609704660f4ed98484da5d8
nitram509 <maki@bitkings.de>
2013-11-26 15:42:54 +0100

    ADD: invoking methods not only uses codeRegisterGroups, but also some other grammar - reason is still not clear, but grammar fixed Sample: "    invoke-direct/range {p0 .. p0}, Lnet/example/monitor;->checkBeforeAction()V    invoke-static/range {v16 .. v16}, Ljava/lang/Integer;-> "

--- smali2java/src/com/litecoding/smali2java/parser/Rule_cmdInvokeDirectRange.java
+++ smali2java/src/com/litecoding/smali2java/parser/Rule_cmdInvokeDirectRange.java
@@ -3,7 +3,7 @@
  * -----------------------------------------------------------------------------
  *
  * Producer : com.parse2.aparse.Parser 2.3
- * Produced : Tue Nov 26 15:38:10 CET 2013
+ * Produced : Tue Nov 26 15:42:15 CET 2013
  *
  * -----------------------------------------------------------------------------
  */
@@ -91,12 +91,64 @@ final public class Rule_cmdInvokeDirectRange extends Rule
           int c1 = 0;
           for (int i1 = 0; i1 < 1 && f1; i1++)
           {
-            rule = Rule_codeRegisterGroup.parse(context);
-            if ((f1 = rule != null))
+            int g1 = context.index;
+            parsed = false;
+            if (!parsed)
             {
-              e1.add(rule);
-              c1++;
+              {
+                ArrayList<Rule> e2 = new ArrayList<Rule>();
+                int s2 = context.index;
+                parsed = true;
+                if (parsed)
+                {
+                  boolean f2 = true;
+                  int c2 = 0;
+                  for (int i2 = 0; i2 < 1 && f2; i2++)
+                  {
+                    rule = Rule_codeRegisterGroup.parse(context);
+                    if ((f2 = rule != null))
+                    {
+                      e2.add(rule);
+                      c2++;
+                    }
+                  }
+                  parsed = c2 == 1;
+                }
+                if (parsed)
+                  e1.addAll(e2);
+                else
+                  context.index = s2;
+              }
             }
+            if (!parsed)
+            {
+              {
+                ArrayList<Rule> e2 = new ArrayList<Rule>();
+                int s2 = context.index;
+                parsed = true;
+                if (parsed)
+                {
+                  boolean f2 = true;
+                  int c2 = 0;
+                  for (int i2 = 0; i2 < 1 && f2; i2++)
+                  {
+                    rule = Rule_codeRegisterRange.parse(context);
+                    if ((f2 = rule != null))
+                    {
+                      e2.add(rule);
+                      c2++;
+                    }
+                  }
+                  parsed = c2 == 1;
+                }
+                if (parsed)
+                  e1.addAll(e2);
+                else
+                  context.index = s2;
+              }
+            }
+            f1 = context.index > g1;
+            if (parsed) c1++;
           }
           parsed = c1 == 1;
         }
